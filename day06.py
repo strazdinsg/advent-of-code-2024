327f33c04986bdb63bf9d9e8ae5713a3b5d0bb96
Girts Strazdins <girts.strazdins@gmail.com>
2024-12-06 08:06:54 +0100

    WIP - try more fancy obstacle placing, still incorrect

--- day06.py
+++ day06.py
@@ -1,4 +1,4 @@
-use_example_input = False
+use_example_input = True
 
 UP = 0
 RIGHT = 1
@@ -18,12 +18,12 @@ def main():
     rows = len(map)
     cols = len(map[0])
     pos = get_guard_position()
-    while guard_within_bounds(pos):
+    while is_within_bounds(pos):
         mark_location(pos)
         pos = move(pos)
     visited = count_visited()
     print(visited)
-    # print_map()
+    print_map()
     obstacles = count_loop_obstacles()
     print(obstacles)
 
@@ -34,7 +34,7 @@ def get_guard_position():
             if map[i][j] == '^':
                 return (i, j, UP)
 
-def guard_within_bounds(pos):
+def is_within_bounds(pos):
     return pos[0] >= 0 and pos[0] < rows and pos[1] >= 0 and pos[1] < cols
 
 def mark_location(pos):
@@ -60,7 +60,7 @@ def get_next_pos(pos):
 
 def is_wall(pos):
     global map
-    return guard_within_bounds(pos) and map[pos[0]][pos[1]] == '#'
+    return is_within_bounds(pos) and map[pos[0]][pos[1]] == '#'
 
 def turn(pos):
     return (pos[0], pos[1], (pos[2] + 1) % 4)
@@ -81,30 +81,34 @@ def count_loop_obstacles():
     for row in range(rows):
         for col in range(cols):
             # Try to place an obstacle on the left
-            if is_dir(row, col, LEFT) and row >= 2 and col >= 1:
-                if is_dir(row - 1, col, UP) or (is_dir(row - 1, col, RIGHT) and is_wall((row - 2, col, RIGHT)) or (is_dir(row - 2, col, UP))):
-                    # print(f"Putting obstacle at ({row}, {col-1})")
-                    obstacles += 1
+            if is_dir((row, col, LEFT)) and gets_into_loop((row - 1, col, UP)):
+                print(f"Putting obstacle at ({row}, {col-1})")
+                obstacles += 1
             # Try to place an obstacle on the bottom
-            if is_dir(row, col, DOWN) and row < rows - 1 and col >= 2:
-                if is_dir(row, col - 1, LEFT) or (is_dir(row, col - 1, UP) and is_wall((row, col - 2, UP)) or (is_dir(row, col - 2, LEFT))):
-                    # print(f"Putting obstacle at ({row+1}, {col})")
-                    obstacles += 1
+            if is_dir((row, col, DOWN)) and gets_into_loop((row, col - 1, LEFT)):
+                print(f"Putting obstacle at ({row+1}, {col})")
+                obstacles += 1
             # Try to place an obstacle on the right
-            if is_dir(row, col, RIGHT) and row < rows - 2 and col < cols - 1:
-                if is_dir(row + 1, col, DOWN) or (is_dir(row + 1, col, LEFT) and is_wall((row + 2, col, LEFT)) or (is_dir(row + 2, col, DOWN))):
-                    # print(f"Putting obstacle at ({row}, {col+1})")
-                    obstacles += 1
+            if is_dir((row, col, RIGHT)) and gets_into_loop((row + 1, col, DOWN)):
+                print(f"Putting obstacle at ({row}, {col+1})")
+                obstacles += 1
             # Try to place an obstacle on the top
-            if is_dir(row, col, UP) and row >= 1 and col < cols - 2:
-                if is_dir(row, col + 1, RIGHT) or (is_dir(row, col + 1, DOWN) and is_wall((row, col + 2, DOWN)) or (is_dir(row, col + 2, RIGHT))):
-                    # print(f"Putting obstacle at ({row-1}, {col})")
-                    obstacles += 1
+            if is_dir((row, col, UP)) and gets_into_loop((row, col + 1, RIGHT)):
+                print(f"Putting obstacle at ({row-1}, {col})")
+                obstacles += 1
     return obstacles
 
-def is_dir(row, col, direction):
+def is_dir(pos):
     global map
-    return map[row][col] == str(direction)
+    return map[pos[0]][pos[1]] == str(pos[2])
+
+def gets_into_loop(pos):
+    global map
+    while is_within_bounds(pos):
+        if is_dir(pos):
+            return True
+        pos = move(pos)
+    return False
 
 def read_input():
     global map
