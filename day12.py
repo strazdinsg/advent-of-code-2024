173d40bd01d10a9598c90c146b6dd3e293ec1790
Girts Strazdins <girts.strazdins@gmail.com>
2024-12-13 07:48:39 +0100

    Day 12, part 2 WIP

--- day12.py
+++ day12.py
@@ -1,13 +1,19 @@
-use_example_input = False
+use_example_input = True
 
 def main():
     garden = read_input()
-    # print_garden(garden)
+    print_garden(garden)
     regions = find_regions(garden)
-    total_price = 0
+    simple_price = 0
     for region in regions:
-        total_price += get_price(region)
-    print("Part 1:", total_price)
+        simple_price += region.get_price()
+    print("Part 1:", simple_price)
+    discounted_price = 0
+    for region in regions:
+        dp = region.get_discounted_price()
+        print(dp)
+        discounted_price += dp
+    print("Part 2:", discounted_price)
 
 def read_input():
     file_name = "example12.txt" if use_example_input else "input12.txt"
@@ -21,6 +27,83 @@ def read_input():
 
 VISITED = "."
 
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+DUP = (-1, 0)
+DRIGHT = (0, 1)
+DDOWN = (1, 0)
+DLEFT = (0, -1)
+DIRECTIONS = {
+    UP: DUP, 
+    RIGHT: DRIGHT,
+    DOWN: DDOWN,
+    LEFT: DLEFT
+}
+OUTSIDE = {
+    UP: DLEFT, 
+    RIGHT: DUP,
+    DOWN: DRIGHT,
+    LEFT: DDOWN
+}
+
+
+class Region:
+    def __init__(self) -> None:
+        self.top_left = None
+        self.plots = {}
+
+    def add_plot(self, pos, fences) -> None:
+        if self.top_left is None:
+            self.top_left = pos
+        self.plots[pos] = fences
+
+    def get_price(self):
+        return self.get_area() * self.get_perimeter()
+
+    def get_area(self):
+        return len(self.plots)
+
+    def get_perimeter(self):
+        perimeter = 0
+        for pos in self.plots:
+            perimeter += len(self.plots[pos])
+        return perimeter
+
+    def get_discounted_price(self):
+        return self.get_area() * self.get_side_count()
+    
+    def get_side_count(self):
+        sides = 0
+        pos = self.top_left
+        dir = RIGHT
+        while True:
+            pos = self.find_last_pos_on_contour(pos, dir)
+            sides += 1
+            if self.has_outside_plot(pos, dir):
+                dir = turn_counter_clockwise(dir)
+                pos = get_next_pos(pos, dir)
+            else:
+                dir = turn_clockwise(dir)
+            if pos == self.top_left and dir == RIGHT:
+                break
+        return sides
+
+    def find_last_pos_on_contour(self, pos, dir):
+        next = get_next_pos(pos, dir)
+        while next in self.plots and not self.has_outside_plot(pos, dir):
+            pos = next
+            next = get_next_pos(pos, dir) 
+        return pos
+    
+    def has_outside_plot(self, pos, dir):
+        outside = get_outside_pos(pos, dir)
+        return outside in self.plots
+
+
+
 def find_regions(garden):
     regions = []
     for i in range(len(garden)):
@@ -29,29 +112,25 @@ def find_regions(garden):
                 regions.append(find_region(garden, i, j))
     return regions
 
-UP = (-1, 0)
-RIGHT = (0, 1)
-DOWN = (1, 0)
-LEFT = (0, -1)
-DIRECTIONS = [UP, RIGHT, DOWN, LEFT]
 
 def find_region(garden, i, j):
     c = garden[i][j]
-    region = []
+    region = Region()
     to_visit = [(i, j)]
     visited = set()
     while to_visit:
-        (i, j) = to_visit.pop()
-        visited.add((i, j))
-        fence_count = 0
-        for dir in DIRECTIONS:
-            next = (i + dir[0], j + dir[1])
+        pos = to_visit.pop()
+        visited.add(pos)
+        fences = set()
+        for direction in DIRECTIONS:
+            d = DIRECTIONS[direction]
+            next = (pos[0] + d[0], pos[1] + d[1])
             if is_same_region(garden, next, c):
                 if next not in visited and next not in to_visit:
                     to_visit.append(next)
             else:
-                fence_count += 1
-        region.append((i, j, fence_count))
+                fences.add(direction)
+        region.add_plot(pos, fences)
     for v in visited:
         garden[v[0]][v[1]] = VISITED
     return region
@@ -65,12 +144,21 @@ def print_garden(garden):
     for line in garden:
         print("".join(line))
 
-def get_price(region):
-    perimeter = 0
-    for plot in region:
-        perimeter += plot[2]
-    area = len(region)
-    return area * perimeter
+def turn_counter_clockwise(dir):
+    return (dir - 1) % 4
+
+def turn_clockwise(dir):
+    return (dir + 1) % 4
+
+def get_next_pos(pos, dir):
+    next_dir = DIRECTIONS[dir]
+    return (pos[0] + next_dir[0], pos[1] + next_dir[1]) 
+
+
+def get_outside_pos(pos: tuple[int, int], dir: int):
+    outside_dir = OUTSIDE[dir]
+    return (pos[0] + outside_dir[0], pos[1] + outside_dir[1])
+
 
 if __name__ == "__main__":
     main()
